07ded277e826ae220fe31776b22c79576e603f93
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-03-21 14:57:15 +0530

    tailwind configuration completed

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function History() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
-      <Text className="text-blue-500 text-2xl font-bold p-4">
+      <Text className="text-green-500 text-2xl font-bold p-4">
        history
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}"
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
